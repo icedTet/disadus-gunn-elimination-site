5faf1b72ac9227979e59ac7fbbc68910259fb877
John Li (Tet) <tet@tet.moe>
2022-02-21 22:44:52 -0800

    changed env?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    ELIM_KEY: process.env.ELIM_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
