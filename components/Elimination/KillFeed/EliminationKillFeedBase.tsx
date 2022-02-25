826ea85741d08f833021c08b349519501d00bc7a
John Li (Tet) <tet@tet.moe>
2022-02-24 17:47:05 -0800

    removed Vlizing the kill feed

--- components/Elimination/KillFeed/EliminationKillFeedBase.tsx
+++ components/Elimination/KillFeed/EliminationKillFeedBase.tsx
@@ -17,34 +17,9 @@ export const EliminationKillFeedBase = (props: { gameID?: string }) => {
         className={`w-full h-full top-0 left-0 absolute overflow-y-auto scrollbar-none rounded-lg bg-gray-100 dark:bg-gray-750 p-4 md:rounded-none md:!bg-transparent md:pt-12`}
         ref={parentRef}
       >
-        <div
-          className={`relative w-full `}
-          style={{
-            height: `${rowVirtualizer.totalSize}px`,
-          }}
-        >
-          {rowVirtualizer.virtualItems.map((virtualRow) => (
-            <div
-              ref={virtualRow.measureRef}
-              key={`${killfeed![virtualRow.index].at}${
-                killfeed![virtualRow.index].entity
-              }${killfeed![virtualRow.index].target}`}
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                // height: `${virtualRow.size}px`,
-                transform: `translateY(${virtualRow.start}px)`,
-              }}
-            >
-              <EliminationKillFeedEntry
-                killItem={killfeed![virtualRow.index]}
-                index={virtualRow.index}
-              />
-            </div>
-          ))}
-        </div>
+        {killfeed?.map((killfeedEntry, index) => (
+          <EliminationKillFeedEntry killItem={killfeed![index]} index={index} />
+        ))}
       </div>
     </div>
   );
